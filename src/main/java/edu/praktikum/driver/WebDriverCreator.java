74a1b9a0634b3e0ddf084c8483744c7d984400ad
Вовк Алексей Анатольевич <avovk@samokat.ru>
2024-07-17 20:13:52 +0300

    remove link

--- src/main/java/edu/praktikum/driver/WebDriverCreator.java
+++ src/main/java/edu/praktikum/driver/WebDriverCreator.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-// https://practicum.yandex.ru/webinar-form/qa-automation-engineer-java/
 public class WebDriverCreator {
 
     /*
